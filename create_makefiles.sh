6a066a234c3564f969200f1c86c98a7b0b665647
Andrew J Ford <andrew.ford@lambdatechllc.com>
2014-06-12 22:36:10 -0400

    Improving script so that it can be executed from any directory.

--- create_makefiles.sh
+++ create_makefiles.sh
@@ -1,6 +1,20 @@
 #!/bin/sh
 
-top_dir=$PWD
+abspath () {
+    path=$1
+    directory=$(dirname $path)
+    filename=$(basename $path)
+
+    if [ -d $path ] ; then
+        path=$(cd $directory && pwd)
+    else
+        path="$(cd $directory && pwd)/$filename"
+    fi
+    echo "$path"
+}
+
+
+top_dir=$(dirname $(abspath $0))
 config_dir=$(cd $top_dir/config && pwd)
 for d in $(find -P . -mindepth 1 -type d) ; do
     if [ ! -e "$d/Makefile" -a -e "$d/software.mk" ] ; then
