fcde85d64a128c66bf4af32bcba1c939bd7ae9cb
Andrew J Ford <andrew.ford@lambdatechllc.com>
2014-12-20 11:36:35 -0500

    Beefing up unit tests for translator.

--- application/unit_tests/messaging/dispatcher_test.cpp
+++ application/unit_tests/messaging/dispatcher_test.cpp
@@ -72,6 +72,20 @@ TYPED_TEST_P(dispatcher_tests, add_message_handler_given_a_message_translator_wi
 }
 
 
+TYPED_TEST_P(dispatcher_tests, add_message_handler_given_a_message_translator_of_same_translation_should_return_failed_to_add_handler) {
+   // Arrange
+   auto target = this->create_target();
+   std::error_code expected{make_error_code(dispatching_error::failed_to_add_handler)};
+   target.add_message_handler([](int){ return double{}; });
+
+   // Act
+   std::error_code actual = target.add_message_handler([](int){ return double{}; });
+
+   // Assert
+   ASSERT_EQ(expected, actual);
+}
+
+
 TYPED_TEST_P(dispatcher_tests, dispatch_message_given_no_handlers_should_return_no_handlers_found) {
    // Arrange
    auto target = this->create_target();
@@ -184,6 +198,51 @@ TYPED_TEST_P(dispatcher_tests, dispatch_message_given_translator_and_single_hand
 }
 
 
+TYPED_TEST_P(dispatcher_tests, dispatch_message_given_translator_with_filter_and_single_handler_should_invoke_the_filter) {
+   // Arrange
+   auto target = this->create_target();
+   bool invoked = false;
+   target.add_message_handler([](double){});
+   target.add_message_handler([](int i){ return static_cast<double>(i); }, [&](int) { invoked = true; return true; });
+
+   // Act
+   target.dispatch_message(std::rand());
+
+   // Assert
+   ASSERT_TRUE(invoked);
+}
+
+
+TYPED_TEST_P(dispatcher_tests, dispatch_message_given_translator_with_a_passing_filter_and_single_handler_should_invoke_translator) {
+   // Arrange
+   auto target = this->create_target();
+   bool invoked = false;
+   target.add_message_handler([](double){});
+   target.add_message_handler([&](int i){ invoked = true; return static_cast<double>(i); }, [](int) { return true; });
+
+   // Act
+   target.dispatch_message(std::rand());
+
+   // Assert
+   ASSERT_TRUE(invoked);
+}
+
+
+TYPED_TEST_P(dispatcher_tests, dispatch_message_given_translator_with_a_failing_filter_and_single_handler_should_not_invoke_translator) {
+   // Arrange
+   auto target = this->create_target();
+   bool invoked = false;
+   target.add_message_handler([](double){});
+   target.add_message_handler([&](int i){ invoked = true; return static_cast<double>(i); }, [](int) { return false; });
+
+   // Act
+   target.dispatch_message(std::rand());
+
+   // Assert
+   ASSERT_FALSE(invoked);
+}
+
+
 TYPED_TEST_P(dispatcher_tests, dispatch_message_given_translator_and_single_handler_should_return_success) {
    // Arrange
    auto target = this->create_target();
@@ -236,6 +295,7 @@ REGISTER_TYPED_TEST_CASE_P(dispatcher_tests,
                            add_message_handler_given_a_message_handler_with_filter_should_return_success,
                            add_message_handler_given_a_message_translator_should_return_success,
                            add_message_handler_given_a_message_translator_with_filter_should_return_success,
+                           add_message_handler_given_a_message_translator_of_same_translation_should_return_failed_to_add_handler,
                            dispatch_message_given_no_handlers_should_return_no_handlers_found,
                            dispatch_message_given_a_message_translator_but_no_handlers_should_return_no_handlers_found,
                            dispatch_message_given_single_handler_should_return_success,
@@ -245,6 +305,9 @@ REGISTER_TYPED_TEST_CASE_P(dispatcher_tests,
                            dispatch_message_given_single_handler_should_invoke_each_handler,
                            dispatch_message_given_translator_and_single_handler_should_invoke_each_handler,
                            dispatch_message_given_translator_and_single_handler_should_return_success,
+                           dispatch_message_given_translator_with_filter_and_single_handler_should_invoke_the_filter,
+                           dispatch_message_given_translator_with_a_passing_filter_and_single_handler_should_invoke_translator,
+                           dispatch_message_given_translator_with_a_failing_filter_and_single_handler_should_not_invoke_translator,
                            dispatch_message_given_multiple_handlers_of_different_types_should_return_success,
                            dispatch_message_given_multiple_handlers_of_different_types_should_invoke_correct_handler);
 
@@ -325,8 +388,38 @@ TYPED_TEST_P(single_handler_dispatcher_tests, add_message_handler_given_multiple
    ASSERT_EQ(expected, actual);
 }
 
+
+TYPED_TEST_P(single_handler_dispatcher_tests, add_message_handler_given_single_handler_and_existing_translator_of_same_type_should_return_success) {
+   // Arrange
+   auto target = this->create_target();
+   std::error_code expected{make_error_code(dispatching_error::none)};
+   target.add_message_handler([](int i){ return static_cast<double>(i); });
+
+   // Act
+   std::error_code actual = target.add_message_handler([](const int){});
+
+   // Assert
+   ASSERT_EQ(expected, actual);
+}
+
+
+TYPED_TEST_P(single_handler_dispatcher_tests, add_message_handler_given_single_translator_and_existing_handler_of_same_type_should_return_success) {
+   // Arrange
+   auto target = this->create_target();
+   std::error_code expected{make_error_code(dispatching_error::none)};
+   target.add_message_handler([](const int){});
+
+   // Act
+   std::error_code actual = target.add_message_handler([](int i){ return static_cast<double>(i); });
+
+   // Assert
+   ASSERT_EQ(expected, actual);
+}
+
 REGISTER_TYPED_TEST_CASE_P(single_handler_dispatcher_tests,
-                           add_message_handler_given_multiple_handlers_of_same_type_should_return_failed_to_add_handler);
+                           add_message_handler_given_multiple_handlers_of_same_type_should_return_failed_to_add_handler,
+                           add_message_handler_given_single_handler_and_existing_translator_of_same_type_should_return_success,
+                           add_message_handler_given_single_translator_and_existing_handler_of_same_type_should_return_success);
 
 typedef ::testing::Types<use_map, use_unordered_map> single_test_types;
 INSTANTIATE_TYPED_TEST_CASE_P(messaging_test_suite, single_handler_dispatcher_tests, single_test_types);
