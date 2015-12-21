de741b806329386316c7e41c476d32a374bcdff0
Andrew J Ford <andrew.ford@lambdatechllc.com>
2015-12-20 20:14:10 -0500

    Get messaging tests passing with conan

--- conanfile.py
+++ conanfile.py
@@ -10,29 +10,29 @@ class Cddd(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
     options = {"static": [True, False], "enable_testing": [True, False]}
-    default_options = "static=True"
+    default_options = "static=True", "gtest:shared=False"
 
     def config(self):
         if (self.settings.build_type == "Debug"):
             self.options.enable_testing = True
 
-        if (self.options.enable_testing):
+        if (hasattr(self.options, "enable_testing") and self.options["enable_testing"]):
             self.requires("gtest/1.7.0@lasote/stable")
 
     def build(self):
         cmake = CMake(self.settings)
         link_type = "-DBUILD_SHARED_LIBS=ON" if not self.options.static else ""
-        lto_option = "-DGCC_LINK_TIME_OPTIMIZATION=ON" if self.lto_enabled else ""
-        enable_testing = "-DBUILD_TESTS=ON" if self.options.enable_testing else ""
+        lto_option = "-DGCC_LINK_TIME_OPTIMIZATION=ON" if self._lto_enabled else ""
+        enable_testing = "-DBUILD_TESTS=ON" if self._testing_enabled else ""
         extra_flags = " ".join([x for x in [link_type, lto_option, enable_testing] if x])
         cwd = getcwd()
         build_dir = join(cwd, "build", str(self.settings.build_type))
 
-        if not exists(build_dir):
+        if not exists(join(build_dir, "CMakeCache.txt")):
             mkdir(build_dir)
-            print "cd %s && cmake %s %s %s" % (build_dir, cmake.command_line, extra_flags, cwd)
-            self.run("cd %s && cmake %s %s %s" % (build_dir, cmake.command_line, extra_flags, cwd))
-        print "cmake --build %s %s" % (build_dir, cmake.build_config)
+            self.output.info("cd %s && cmake %s %s %s" % (build_dir, self._directory_of_self, cmake.command_line, extra_flags))
+            self.run("cd %s && cmake %s %s %s" % (build_dir, self._directory_of_self, cmake.command_line, extra_flags))
+        self.output.info("cmake --build %s %s" % (build_dir, cmake.build_config))
         self.run("cmake --build %s %s" % (build_dir, cmake.build_config))
 
     def package_info(self):
@@ -41,7 +41,11 @@ class Cddd(ConanFile):
             self._setup_gcc()
 
     @property
-    def lto_enabled(self):
+    def _testing_enabled(self):
+        return hasattr(self.options, "enable_testing") and self.options["enable_testing"]
+
+    @property
+    def _lto_enabled(self):
         return self.settings.compiler == "gcc" and \
                 self.settings.build_type == "Release" and \
                 self.options.static
@@ -49,7 +53,6 @@ class Cddd(ConanFile):
     @property
     def _directory_of_self(self):
         return dirname(abspath(__file__))
-
 
     def _setup_gcc(self):
         self.cpp_info.cppflags.append("-Wall")
