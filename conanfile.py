1d3ede19f0104554dadf6590bd914a0f47690549
Andrew J Ford <andrew.ford@lambdatechllc.com>
2015-12-22 19:29:12 -0500

    Integrate with kerchow for testing

--- conanfile.py
+++ conanfile.py
@@ -5,58 +5,70 @@ from os.path import join, exists, dirname, abspath
 
 class Cddd(ConanFile):
     name = "cddd"
-    version = "0.1"
+    version = "0.1.0"
     url = "https://github.com/skizzay/cddd.git"
+    requires = "Boost/1.60.0@lasote/stable"
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
-    options = {"static": [True, False], "enable_testing": [True, False]}
-    default_options = "static=True", "gtest:shared=False"
+    options = {"shared": [False, True], "build_tests": [True, False]}
+    default_options = "shared=False"
 
     def config(self):
-        if (self.settings.build_type == "Debug"):
-            self.options.enable_testing = True
+        # Debug builds will build the tests by default
+        if not hasattr(self.options, "build_tests"):
+            self.options["build_tests"] = self.settings.build_type == "Debug"
+        self.output.info("We are%s building tests" % ("" if self.options.build_tests else " not"))
 
-        if (hasattr(self.options, "enable_testing") and self.options["enable_testing"]):
+    def requirements(self):
+        if self.options.build_tests:
+            self.requires("FakeIt/2.0.3@skizzay/testing")
+            self.options["FakeIt"].framework = "standalone"
+            self.requires("kerchow/1.0.1@skizzay/stable")
+            self.options["kerchow"].shared = self.options.shared
             self.requires("gtest/1.7.0@lasote/stable")
+            self.options["gtest"].shared = self.options.shared
 
     def build(self):
         cmake = CMake(self.settings)
-        link_type = "-DBUILD_SHARED_LIBS=ON" if not self.options.static else ""
-        lto_option = "-DGCC_LINK_TIME_OPTIMIZATION=ON" if self._lto_enabled else ""
-        enable_testing = "-DBUILD_TESTS=ON" if self._testing_enabled else ""
-        extra_flags = " ".join([x for x in [link_type, lto_option, enable_testing] if x])
-        cwd = getcwd()
-        build_dir = join(cwd, "build", str(self.settings.build_type))
+        self.output.info("cmake %s %s %s" % (self._directory_of_self, cmake.command_line, self._extra_cmake_flags))
+        self.run("cmake %s %s %s" % (self._directory_of_self, cmake.command_line, self._extra_cmake_flags))
+        self.output.info("cmake --build . %s" % cmake.build_config)
+        self.run("cmake --build . %s" % cmake.build_config)
 
-        if not exists(join(build_dir, "CMakeCache.txt")):
-            mkdir(build_dir)
-            self.output.info("cd %s && cmake %s %s %s" % (build_dir, self._directory_of_self, cmake.command_line, extra_flags))
-            self.run("cd %s && cmake %s %s %s" % (build_dir, self._directory_of_self, cmake.command_line, extra_flags))
-        self.output.info("cmake --build %s %s" % (build_dir, cmake.build_config))
-        self.run("cmake --build %s %s" % (build_dir, cmake.build_config))
+        if self.options.build_tests:
+            self.run("ctest")
 
     def package_info(self):
-        self.cpp_info.libs.append("cddd")
-        if self.settings.compiler == "Gcc":
+        self.cpp_info.libs = ["cddd"]
+        if self.settings.compiler == "gcc":
             self._setup_gcc()
 
     @property
-    def _testing_enabled(self):
-        return hasattr(self.options, "enable_testing") and self.options["enable_testing"]
+    def _build_tests_flag(self):
+        return "-DBUILD_TESTS=TRUE" if self.options.build_tests else ""
+
+    @property
+    def _link_type_flag(self):
+        return "-DBUILD_SHARED_LIBS=ON" if self.options.shared else ""
+
+    @property
+    def _lto_flag(self):
+        return "-DGCC_LINK_TIME_OPTIMIZATION=ON" if self._lto_enabled else ""
+
+    @property
+    def _extra_cmake_flags(self):
+        return " ".join([x for x in [self._build_tests_flag, self._link_type_flag, self._lto_flag] if x])
 
     @property
     def _lto_enabled(self):
         return self.settings.compiler == "gcc" and \
                 self.settings.build_type == "Release" and \
-                self.options.static
+                not self.options.shared
 
     @property
     def _directory_of_self(self):
         return dirname(abspath(__file__))
 
     def _setup_gcc(self):
-        self.cpp_info.cppflags.append("-Wall")
-        self.cpp_info.cppflags.append("-Wextra")
-        self.cpp_info.cppflags.append("-Werror")
         if self.settings.compiler.version.major >= 5:
             self.cpp_info.cppflags.append("-std=c++1z")
